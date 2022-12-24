879f8df0e95326a2280e6ffde708a8fcbec17d57
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-24 11:50:05 +0530

    Update header.component.ts

--- src/app/modules/shared/layout/header/header.component.ts
+++ src/app/modules/shared/layout/header/header.component.ts
@@ -8,12 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input()
-  title : string | undefined;
+  title: string | undefined;
 
   constructor() { }
 
   ngOnInit() {
-    console.log('ngOnInit is called in header.component.ts')
+    console.log('ngOnInit is called in header.component.ts');
+
+    // Initialization should be done in ngOnInit()
+    // This value will over ride the value in app.component.ts sent as input to this component
+    // this.title = 'Title'; 
   }
 
 }
