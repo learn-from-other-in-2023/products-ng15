1f1bc7844cf0cb7f688fa6dc2e086090a43bfe5e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-24 11:02:15 +0530

    Organization of Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { HeaderComponent } from './modules/shared/layout/header/header.component';
+import { SharedModule } from './modules/shared/shared.module';
+import { NgMaterialModule } from './modules/shared/ngmaterial.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule
+    SharedModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
