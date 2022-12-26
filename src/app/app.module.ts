7d6b95396272fe3fa9a7e085ca2aba746a74001d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-26 16:11:52 +0530

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './modules/shared/shared.module';
 import { NgMaterialModule } from './ngmaterial/ngmaterial.module';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
